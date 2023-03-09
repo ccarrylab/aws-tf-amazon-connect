36f031f941cc8fbdf7d46ec40d4c0d71917ffd75
Cohen Carryl <cohen.carryl@gmail.com>
2023-03-09 12:11:13 -0500

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -1,4 +1,4 @@
 module "connect-instance" {
-  source                 = "git::https://github.com/fortunecookiezen/aws-tf-amazon-connect.git?ref=v2.0"
+  source                 = "git::https://github.com/ccarrylab/aws-tf-amazon-connect.git?ref=v2.0"
   connect-instance-alias = "demo"
 }
